e911db123ab4cdd0e889deba2928165b7eeba790
usrlocal <usrlocal@Gfixeetu234.ad.univ-angers.fr>
2018-12-12 17:30:27 +0100

    toolbar

--- app/src/main/java/com/example/usrlocal/projetmobile/MainActivity.java
+++ app/src/main/java/com/example/usrlocal/projetmobile/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.usrlocal.projetmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.toolbar);
+        if (toolbar != null)
+        {
+            setSupportActionBar(toolbar);
+        }
+
+     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_stats:
+                Toast.makeText(getApplicationContext(),"Stats",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_menu:
+                Toast.makeText(getApplicationContext(),"Menu",Toast.LENGTH_SHORT).show();
+                break;
+                 }
+        return super.onOptionsItemSelected(item);
     }
 }
