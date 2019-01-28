401b62a0ccfc922ad3ab83049269df305d537072
qchouteau <quentin.chouteau@live.fr>
2019-01-28 19:51:56 +0100

    modif

--- app/src/main/java/com/example/usrlocal/projetmobile/MenuActivity.java
+++ app/src/main/java/com/example/usrlocal/projetmobile/MenuActivity.java
@@ -1,25 +1,21 @@
 package com.example.usrlocal.projetmobile;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.prefs.PreferenceChangeEvent;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     Button btnSimple = null;
     Button btnMedium = null;
@@ -35,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         ListbtnDifficulty = new ArrayList<>();
 
@@ -49,14 +45,6 @@ public class MainActivity extends AppCompatActivity {
         etPseudo = (EditText) findViewById(R.id.etPseudo);
         tvBjr = (TextView) findViewById(R.id.tvBjr);
 
-
-       /* boutonGame8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchGame(8);
-
-            }
-        });*/
 
         btnSimple.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,31 +101,19 @@ public class MainActivity extends AppCompatActivity {
             String pseudo = etPseudo.getText().toString();
             Toast.makeText(getApplicationContext(), pseudo, Toast.LENGTH_SHORT).show();
 
-            /*if (pseudo.length() == 0) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage("Veuillez entrer un pseudo pour l'enregistrement.");
-                builder.setCancelable(false);
-                builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                });
-                builder.create().show();
-            } else {*/
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("PSEUDO", pseudo);
             editor.putInt("taille", taille);
             editor.apply();
             Toast.makeText(getApplicationContext(), "game : " + taille, Toast.LENGTH_SHORT).show();
-                /*intentGame = new Intent(MainActivity.this, ActivityGame.class);
-                intentGame.putExtra("taille",taille);
-                startActivity(intentGame);*/
+            /*intentGame = new Intent(MenuActivity.this, ActivityGame.class);
+             intentGame.putExtra("taille",taille);
+             startActivity(intentGame);*/
 
             }
         else {
-            /*intentGame = new Intent(MainActivity.this, ActivityGame.class);
+            /*intentGame = new Intent(MenuActivity.this, ActivityGame.class);
             intentGame.putExtra("taille",taille);
             startActivity(intentGame);*/
             Toast.makeText(getApplicationContext(), "game : " + taille, Toast.LENGTH_SHORT).show();
