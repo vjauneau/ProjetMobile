7f29b0b1b76f5683665c059c81659e8f02939be7
courson1 <44193005+courson1@users.noreply.github.com>
2019-01-30 00:08:26 +0100

    update

--- app/src/main/java/com/example/usrlocal/projetmobile/TabStatisticsFragment.java
+++ app/src/main/java/com/example/usrlocal/projetmobile/TabStatisticsFragment.java
@@ -1,13 +1,16 @@
 package com.example.usrlocal.projetmobile;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +24,12 @@ public class TabStatisticsFragment extends Fragment {
     private int gameSize = 0;
     private TextView gameSizeText = null;
     private View view;
+
+    private ImageView difficultyIcon = null;
+    private TextView pseudoText = null;
+    private TextView bestTimeText = null;
+    private TextView nbGamesText = null;
+    private TextView perCentWinText = null;
 
     public TabStatisticsFragment() {
         // Required empty public constructor
@@ -54,11 +63,37 @@ public class TabStatisticsFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_tab_statistics, container, false);
         //this.gameSizeText.setText(String.valueOf(getGameSize()));
 
+        this.difficultyIcon = (ImageView) view.findViewById(R.id.difficultyIcon);
+        this.pseudoText = (TextView) view.findViewById(R.id.pseudo);
+        this.bestTimeText = (TextView) view.findViewById(R.id.bestTime);
+        this.nbGamesText = (TextView) view.findViewById(R.id.gamePlayed);
+        this.perCentWinText = (TextView) view.findViewById(R.id.perCentWin);
+
         return view;
     }
 
     @Override
     public void onStart() {
         super.onStart();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+
+        // Set difficulty icon.
+        switch (getGameSize()) {
+            case 8:
+                this.difficultyIcon.setImageResource(R.drawable.easy);
+                break;
+            case 12:
+                this.difficultyIcon.setImageResource(R.drawable.medium);
+                break;
+            case 16:
+                this.difficultyIcon.setImageResource(R.drawable.hard);
+                break;
+        }
+
+        // Set player name.
+        this.pseudoText.setText(preferences.getString("PSEUDO",null));
+
+        
     }
 }
